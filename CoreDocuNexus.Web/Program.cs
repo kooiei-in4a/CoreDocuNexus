7aacf373d7dfba5aa6ec3b0fce25268b58ab6905
kouichirou ie <kouichirou.ie@in4a.jp>
2025-06-21 15:26:38 +0900

    MudBlazor導入とUI改善
    - `CoreDocuNexus.Web.csproj`でパッケージバージョンを更新し、`MudBlazor`を追加。
    - `MainLayout.razor`で従来のレイアウトを削除し、`MudBlazor`のプロバイダーを追加。
    - `Home.razor`でページタイトルを変更し、PDFファイルのアップロードとパスワード設定のUIを改善。
    - `TestApi.razor`でURLを変更し、エラーメッセージの変数をnullableに。
    - `Program.cs`で`MudBlazor`サービスを追加。
    - `_Imports.razor`に`MudBlazor`の名前空間を追加。
    - `index.html`でBootstrapを削除し、`MudBlazor`のスタイルシートとGoogle Fontsを追加。

--- CoreDocuNexus.Web/Program.cs
+++ CoreDocuNexus.Web/Program.cs
@@ -1,10 +1,12 @@
+using jp.in4a.CoreDocuNexus.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using jp.in4a.CoreDocuNexus.Web;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "";
 
