2c9941e3ccee072491890fa09468850f80447a92
kouichirou ie <kouichirou.ie@in4a.jp>
2025-06-22 20:20:00 +0900

    appsettings.jsonをrazorで使えるようにDI

--- CoreDocuNexus.Web/Program.cs
+++ CoreDocuNexus.Web/Program.cs
@@ -1,16 +1,20 @@
 using jp.in4a.CoreDocuNexus.Web;
+using jp.in4a.CoreDocuNexus.Web.AppSettings;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddMudServices();
 
-var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "";
+var rootSettings = new RootSettings();
+builder.Configuration.Bind(rootSettings);
+builder.Services.AddSingleton(rootSettings);
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(rootSettings.ApiBaseUrl)});
 
 await builder.Build().RunAsync();
