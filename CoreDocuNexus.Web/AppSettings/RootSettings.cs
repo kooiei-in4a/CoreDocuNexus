73f502927c3871e4168b96b403c74437fa39b24c
kouichirou ie <kouichirou.ie@in4a.jp>
2025-06-28 23:52:33 +0900

    アプリ設定の追加とフッターのデザイン改善
    RootSettings.csにアプリ名、説明、環境名、APIキーなどのプロパティを追加し、環境設定の柔軟性を向上させました。
    MainLayout.razorでアプリ設定をインジェクトし、フッターに動的な情報を表示するように変更しました。
    PassGa.razorでは、ハードコーディングされたテキストを設定プロパティに置き換えました。
    appsettings.Development.json、appsettings.json、appsettings.Production.json、appsettings.Staging.jsonで環境に応じた設定を強化しました。
    app.cssでフッターのスタイルを追加し、デザインを改善しました。

--- CoreDocuNexus.Web/AppSettings/RootSettings.cs
+++ CoreDocuNexus.Web/AppSettings/RootSettings.cs
@@ -2,7 +2,9 @@
 {
     public class RootSettings
     {
-        public string ApiBaseUrl { get; set; } = string.Empty;
+        public string AppName { get; set; } = string.Empty;
+
+        public string AppDescription { get; set; } = string.Empty;
 
         public int MaxUploadFileSize { get; set; } = 10485760; // 10 MB
 
@@ -10,5 +12,17 @@
         /// アップロード最大ファイルサイズ（MB単位）
         /// </summary>
         public double MaxUploadFileSizeMB => MaxUploadFileSize / 1024.0 / 1024.0;
+
+        public string ApiBaseUrl { get; set; } = string.Empty;
+
+
+        public string EnvironmentName { get; set; } = string.Empty;
+
+        public string ApiKey { get; set; } = string.Empty;
+
+        public bool IsDevelopment => EnvironmentName.Equals("Development", StringComparison.OrdinalIgnoreCase);
+        public bool IsStaging => EnvironmentName.Equals("Staging", StringComparison.OrdinalIgnoreCase);
+        public bool IsProduction => EnvironmentName.Equals("Production", StringComparison.OrdinalIgnoreCase);
+
     }
 }
